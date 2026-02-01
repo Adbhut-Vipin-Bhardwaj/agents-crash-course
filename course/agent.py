7756d6bbac4ba83de21385a1b1c5ca9b7f1712a3
Adbhut <56891717+Adbhut-Vipin-Bhardwaj@users.noreply.github.com>
2026-02-01 15:52:17 +0530

    Add agent.py to course

---
+++ course/agent.py
@@ -0,0 +1,33 @@
+import asyncio
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIChatModel
+from pydantic_ai.providers.openai import OpenAIProvider
+
+from search import text_search
+
+system_prompt = """
+You are a helpful assistant for a course. 
+
+Always search for relevant information before answering. 
+If the first search doesn't give you enough information, try different search terms.
+
+Make multiple searches if needed to provide comprehensive answers.
+"""
+
+with open("./openai_api_key.txt", "r") as f:
+    api_key = f.read().strip()
+
+model = OpenAIChatModel(
+    'gpt-4o-mini',
+    provider=OpenAIProvider(api_key=api_key)
+)
+agent = Agent(
+    name="faq_agent",
+    instructions=system_prompt,
+    tools=[text_search],
+    model=model
+)
+
+question = "I just discovered the course, can I join now?"
+
+result = asyncio.run(agent.run(user_prompt=question))
